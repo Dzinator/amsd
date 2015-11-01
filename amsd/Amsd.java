9f1ce8dcd44bb9951b284ba836f7915f93ff36ae
jcaplan <jonah.caplan@mail.mcgill.ca>
2015-11-01 16:09:06 -0500

    automatically load and save previous data on program start and exit

--- amsd/Amsd.java
+++ amsd/Amsd.java
@@ -1,20 +1,37 @@
 package amsd;
 
+import static org.junit.Assert.fail;
+import amsd.model.AppointmentManagementSystem;
 import amsd.persistence.PersistenceAMSD;
+import amsd.persistence.PersistenceXStream;
 import amsd.view.*;
 
 public class Amsd {
 
 	public static void main(String[] args) {
 		// load model
-		//PersistenceAMSD.loadEventRegistrationModel();
-		
+		PersistenceAMSD.loadEventRegistrationModel();
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+        	
+        	public void run(){
+        		AppointmentManagementSystem ams = AppointmentManagementSystem.getInstance();
+//        		PersistenceAMSD pers = new PersistenceAMSD();
+        		PersistenceAMSD.initializeXStream();
+
+        		if (!PersistenceXStream.saveToXMLwithXStream(ams))
+        			fail("Could not save file.");
+        	}
+        	
+        });
+        
 		// start UI
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new BookingPage().setVisible(true);
             }
         });
+
+        
 
 	}
 
